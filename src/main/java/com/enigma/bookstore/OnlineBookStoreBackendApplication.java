7d9c9aa20b62c5ffd173c34aacb43adb06496911
vishal-patil01 <vishalpatil01@hotmail.com>
2020-06-11 15:22:22 +0530

    Refactored : Override Configure Method Of SpringBootServletInitializer

--- src/main/java/com/enigma/bookstore/OnlineBookStoreBackendApplication.java
+++ src/main/java/com/enigma/bookstore/OnlineBookStoreBackendApplication.java
@@ -3,12 +3,18 @@ package com.enigma.bookstore;
 import com.enigma.bookstore.properties.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationProperties.class)
-public class OnlineBookStoreBackendApplication {
+public class OnlineBookStoreBackendApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OnlineBookStoreBackendApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreBackendApplication.class, args);
     }
